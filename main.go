2d11a3f8c87047348fe80fa261ef3c2a9f2a03f7
Sagar Utekar <sagarutekar2366@gmail.com>
2023-07-31 08:35:37 +0530

    Update main.go

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	routes.UserRoutes(router)
 
 	router.GET("/api-1", func(c *gin.Context) {
-		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+		c.JSON(200, gin.H{"success": "Access granted for api-1 ************"})
 	})
 
 	router.GET("/api-2", func(c *gin.Context) {
